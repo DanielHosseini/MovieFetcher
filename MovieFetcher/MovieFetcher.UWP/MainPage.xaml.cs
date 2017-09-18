cf925ef2f811d70e4ea5832d081ec2c54b43564f
Daniel Hosseini <daniel.hosseini1@gmail.com>
2017-09-18 13:11:55 +0200

    new project files

---
+++ MovieFetcher/MovieFetcher.UWP/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using FFImageLoading.Forms.WinUWP;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace MovieFetcher.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            CachedImageRenderer.Init();
+
+            LoadApplication(new MovieFetcher.App());
+        }
+    }
+}
---
+++ MovieFetcher/MovieFetcher/SpecificView.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MovieFetcher
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SpecificView : ContentPage
+    {
+        private Movy _specificMovieObject;
+        public string MovieTitle { get; private set; }
+        public int Year { get; private set; }
+        public string Summary { get; private set; }
+        public double IMDBRating { get; private set; }
+        public string CoverImageLink { get; private set; }
+        public int TotalRunTime { get; set; }
+        public Uri CoverImageUri { get; set; }
+        public string Quality { get; set; }
+        public ImageSource ImdbLogoSource { get; set; }
+        public ImageSource TomatoLogoSource { get; set; }
+        public ImageSource YouTubeLogoSource { get; set; }
+        public ImageSource QualityLogoSource { get; set; }
+        public ImageSource RuntimeLogoSource { get; set; }
+        private TapGestureRecognizer tapGestureRecognizer = new TapGestureRecognizer();
+        public string YoutubeIdCode { get; set; }
+
+        public SpecificView()
+        {
+            InitializeComponent();
+        }
+
+        public SpecificView(Movy specificMovieObject, int movieID) : this()
+        {
+            _specificMovieObject = specificMovieObject;
+            Title = _specificMovieObject.title;
+            Year = _specificMovieObject.year;
+            Summary = _specificMovieObject.summary;
+            IMDBRating = _specificMovieObject.rating;
+            TotalRunTime = _specificMovieObject.runtime;
+            Quality = ReturnQualtiyOfMovie(_specificMovieObject.torrents);
+            CoverImageLink = _specificMovieObject.large_cover_image;
+            CoverImageUri = new Uri(CoverImageLink);
+            YoutubeIdCode = _specificMovieObject.yt_trailer_code;
+            ImdbLogoSource = ImageSource.FromResource("MovieFetcher.Images.imdb128.png");
+            YouTubeLogoSource = ImageSource.FromResource("MovieFetcher.Images.youtube128.png");
+            RuntimeLogoSource = ImageSource.FromResource("MovieFetcher.Images.runtime128.png");
+            QualityLogoSource = ImageSource.FromResource("MovieFetcher.Images.quality128.png");
+            youtubeBtn.GestureRecognizers.Add(tapGestureRecognizer);
+
+            BindingContext = this;
+
+            tapGestureRecognizer.Tapped += (sender, e) =>
+            {
+                Device.OpenUri(new Uri("https://www.youtube.com/watch?v=" + YoutubeIdCode));
+            };
+        }
+
+        private string ReturnQualtiyOfMovie(IList<Torrent> torrents)
+        {
+            string quality = "";
+            foreach (var item in torrents)
+            {
+                quality += item.quality + " ";
+            }
+            return quality;
+        }
+    }
+}
